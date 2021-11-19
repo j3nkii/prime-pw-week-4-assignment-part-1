edb9c0e80a74a4b8522f70327d6ee9ee20aef12b
Jacob Larson <jacob.larson55112@gmail.com>
2021-11-19 12:20:12 -0600

    completed and Reviewed

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -1,4 +1,4 @@
-console.log('***** Function Practice *****')
+console.log('***** Function Practice *****');
 
 // Add the required code to complete the functions below
 // After _each_ function, use a console log to call the function
@@ -9,7 +9,7 @@ function hello() {
   return 'Hello World!';
 }
 // Call the function to test
-console.log('Test - should say "Hello World!"', hello());
+console.log('1: Test - should say "Hello World!"', hello());
 
 
 // 2. Function to return a personalized hello, using the `name` argument.
@@ -18,7 +18,7 @@ function helloName( name ) {
   return `Hello ${name}!`;
 }
 // Remember to call the function to test
-console.log(helloName('Jacob'))
+console.log('2: ', helloName('Jacob'));
 
 // 3. Function to add two numbers together & return the result
 function addNumbers( firstNumber, secondNumber ) {
@@ -26,15 +26,14 @@ function addNumbers( firstNumber, secondNumber ) {
   total = firstNumber + secondNumber;
   return total;
 }
-console.log(addNumbers(69, 420));
+console.log(`3: Should log 489: ${addNumbers(69, 420)}`);
 
 // 4. Function to multiply three numbers & return the result
 function multiplyThree( a, b ,c ){
-  let total;
-  total = a * b * c;
+  let total = a * b * c;
   return total;
 }
-console.log(multiplyThree( 3, 14, 10));
+console.log(`4: Should log 420: ${multiplyThree( 3, 14, 10)}`);
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
@@ -48,37 +47,37 @@ function isPositive( number ) {
 
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
-console.log( 'isPositive - should say true', isPositive(3) );
-console.log( 'isPositive - should say false', isPositive(0) );
-console.log( 'isPositive - should say false', isPositive(-3) );
+console.log( '5: isPositive - should say true', isPositive(3) );
+console.log( '5: isPositive - should say false', isPositive(0) );
+console.log( '5: isPositive - should say false', isPositive(-3) );
 
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
-let urMom = [1, 2, 3, 4]
-let urDad = []
+let urMom = [1, 2, 3, 4];
+let urDad = [];
 function getLast( array ) {
   return array.pop();
 }
-console.log(`Last is: ${getLast(urMom)}`);
-console.log(`Last is: ${getLast(urDad)}`);
+
+console.log(`6: Last is: ${getLast(urMom)}`);
+console.log(urMom);
+console.log(`6: Last is: ${getLast(urDad)}`);
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find( value, array ){
   let found = false;
-  for(index of array) {
-    index === value ? found = true : false;
-    } 
-  if(found === true) {
-    return true;
-  } else {
-    return false;
-  }
+  for(let index of array) {
+    found = index === value ? true : false;
+  } 
+  return found;
 }
-let urGranny = ['Bob', 'Frank', 'Phillip', 'Wallace'];
-console.log(`Found Frank?: ${find('Frank', urGranny)}`);
+
+let urGranny = ['Bob', 'Frank', 'Phillip', 'Wallace', 'Shirley',];
+console.log(`7: Found 'Shirley?: ${find('Shirley', urGranny)}`);
+console.log(`7: Found 'Shirley?: ${find('Dillon', urGranny)}`);
 
 // ----------------------
 // Stretch Goals
@@ -86,24 +85,23 @@ console.log(`Found Frank?: ${find('Frank', urGranny)}`);
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-  let found;
-  letter === string[0] ? found = true : found = false;
+  let found = letter === string[0] ? true : false;
   return found;
 }
-console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
-console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
+console.log( '8: isFirstLetter - should say true', isFirstLetter('a', 'apple') );
+console.log( '8: isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
 let wildArray = [47, 82, 34];
 function sumAll(array ) {
-  let sum = 0
+  let sum = 0;
   // TODO: loop to add items
-  for(i=0; i < array.length; i++) {
+  for(let i=0; i < array.length; i++) {
     sum+=array[i];
   }
   return sum;
 }
-console.log(sumAll(wildArray));
+console.log('9: Should log 163:', sumAll(wildArray));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
@@ -111,16 +109,16 @@ console.log(sumAll(wildArray));
 
 function positives(array) {
   let arrayCopy = [];
-  for(value of array) {
+  for(let value of array) {
     if(value > 0) {
-      arrayCopy.push(value)
+      arrayCopy.push(value);
     }
   }
   return arrayCopy;
 }
 let buttVille = [1, -2, 3, -4, 5];
-console.log(positives(buttVille))
-console.log(buttVille);
+console.log('10: ', positives(buttVille));
+console.log('10: ', buttVille);
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
@@ -139,18 +137,18 @@ console.log('--------next----------');
 //If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
 
 function order(words) {
-  let array = words.split(' ')
+  let array = words.split(' ');
   let result = [];
   for(word of array) {
     for(letter of word) {
       if(isNaN(letter) === false ) {
-        Number.parseInt(letter) - 1 > result.length ? array.push(word) : result.splice( Number.parseInt(letter) - 1, 0, word)
+        Number.parseInt(letter) - 1 > result.length ? array.push(word) : result.splice( Number.parseInt(letter) - 1, 0, word);
       }
     }
   }
   return result.join(' ');
 }
 
-console.log(order("is2 Thi1s T4est 3a"), "Should log: Thi1s is2 3a T4est")
-console.log(order("4of Fo1r pe6ople g3ood th5e the2"), "Should log: Fo1r the2 g3ood 4of th5e pe6ople")
-console.log(order(""), "empty input should return empty string" )
+console.log(`Should log: Thi1s is2 3a T4est: ${order('is2 Thi1s T4est 3a')}`);
+console.log(`Should log: Fo1r the2 g3ood 4of th5e pe6ople ${order('4of Fo1r pe6ople g3ood th5e the2')}`);
+console.log(`Should return empty string followed by type: ${order("")}`, typeof order(""));
